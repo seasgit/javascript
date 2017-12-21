6e0167cb2e3e70b17116c54aa43cab97d1b7e8be
seasone <dev@seasone.fr>
2017-12-21 09:12:44 +0100

    datas diaporama

---
+++ diaporama/assets/js/app.js
@@ -0,0 +1,1 @@
+// à réaliser en cours
---
+++ diaporama/database/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+	class Auth {
+
+		const DSN = 'mysql:host=localhost;charset=utf8;dbname=db_diaporama';
+		const USER = 'root';
+		const PWD = '';
+
+	}
